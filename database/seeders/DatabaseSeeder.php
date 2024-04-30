0b97f1db1349506314ac30e299eb10bb28217b27
simizu-masato <m.shimizu@trigger4.com>
2024-04-30 19:01:55 +0900

    前回のコミットし忘れ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Todo::factory()->count(10)->create();
     }
 }
